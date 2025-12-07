bd7a89d805f89fb51ea0da51bfdec0cd33bd6356
kaos35 <mcobanoglu3434@gmail.com>
2025-12-07 13:21:50 +0100

    fix vercel build: exclude metamask async-storage & pino-pretty

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      "@react-native-async-storage/async-storage": false,
+      "pino-pretty": false,
+      "fs": false,
+      "path": false,
+    };
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
